06deb06c8be15d704166b48f69d5b532723d7288
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2024-01-03 00:58:23 -0300

    Upgrade: Arreglo estetico

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import * as products from "./products.json";
 
 class Product {
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
   id: number;
   name: string;
   price: number;
 
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+  
   static findProductsBelow(minPrice: number): Product[] {
     const productsForJSON = products["default"];
 
@@ -17,16 +18,20 @@ class Product {
 }
 
 class User {
+  name: string;
+  products: Product[] = [];
+
   constructor(name: string) {
     this.name = name;
   }
-  name: string;
-  products: Product[] = [];
+
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
+
   addProducts(newProducts: Product[]) {
     newProducts.forEach((element) => this.addProduct(element));
+    // this.products = this.products.concat(newProduct) -> Que tener más consciencia de los metodos que puede
   }
 }
 
